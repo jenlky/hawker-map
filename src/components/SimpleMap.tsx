39a9be9a478aa1ade8446b1cac76d9b5df56f9a6
Jenssen <jenssen.lee@gmail.com>
2024-06-13 16:53:13 +0800

    Refactor SimpleMap to use export default function

--- src/components/SimpleMap.tsx
+++ src/components/SimpleMap.tsx
@@ -1,24 +1,23 @@
 import L from "leaflet";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, Popup, TileLayer, Tooltip } from "react-leaflet";
 
 const icon = L.icon({ iconUrl: "/images/marker-icon.png" });
 
-const SimpleMap = () => {
+export default function SimpleMap() {
   const coordinates: L.LatLngExpression = [1.3521, 103.8198]
 
   return ( 
-    <MapContainer center={coordinates} zoom={13} scrollWheelZoom={true} style={{ height: '100vh', width: '100wh' }}>
+    <MapContainer center={coordinates} zoom={13} scrollWheelZoom={true} style={{ height: '90vh', width: '90wh' }}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       <Marker position={coordinates} icon={icon}>
-        <Popup>
+        {/* <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
+        </Popup> */}
+        <Tooltip direction='left' offset={[0, 0]} opacity={1} permanent>Testing123</Tooltip>
       </Marker>
     </MapContainer>
   );
 };
-
-export default SimpleMap;
