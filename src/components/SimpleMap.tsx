094c96b867d79d204ff5ea4fbb8ab30f21589b0d
Jenssen <jenssen.lee@gmail.com>
2024-06-13 16:14:17 +0800

    Replace lat and long with coordinates variable

--- src/components/SimpleMap.tsx
+++ src/components/SimpleMap.tsx
@@ -5,16 +5,15 @@ import 'leaflet/dist/leaflet.css';
 const icon = L.icon({ iconUrl: "/images/marker-icon.png" });
 
 const SimpleMap = () => {
-  const latitude = 1.3521;
-  const longitude = 103.8198;
+  const coordinates: L.LatLngExpression = [1.3521, 103.8198]
 
   return ( 
-    <MapContainer center={[latitude, longitude]} zoom={13} scrollWheelZoom={true} style={{ height: '100vh', width: '100wh' }}>
+    <MapContainer center={coordinates} zoom={13} scrollWheelZoom={true} style={{ height: '100vh', width: '100wh' }}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[latitude, longitude]} icon={icon}>
+      <Marker position={coordinates} icon={icon}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
