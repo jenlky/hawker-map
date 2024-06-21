4362877c04f352d4bd2731db2563d5ce20bc2aad
Jenssen <jenssen.lee@gmail.com>
2024-06-21 10:55:47 +0800

    Add redMarker to MarkerWithLabel component

--- src/components/MarkerWithLabel.tsx
+++ src/components/MarkerWithLabel.tsx
@@ -1,13 +1,28 @@
 import L from "leaflet";
-import { Marker, Tooltip } from "react-leaflet";
+import { useState } from "react";
+import { Marker, Tooltip, useMapEvent } from "react-leaflet";
 
-export default function MarkerWithLabel({ data }: any) {
-  const icon = L.icon({ iconUrl: "/images/marker-icon.png" });
+export default function MarkerWithLabel({ data }: { data: any }) {
+  const [isPermanent, setIsPermanent] = useState(false)
+  const blueMarker = L.icon({ iconUrl: "/images/marker-icon.png" });
+  const redMarker = L.icon({ iconUrl: "/images/marker-icon-red.png" });
+
   const { coordinates, name, photoUrl, description, otherWorksStartDate, otherWorksEndDate } = data
 
+  // const MapEvent = () => {
+  //   const map = useMapEvent('moveend', () => {
+  //     const zoom = map.getZoom()
+  //     console.log('getZoom', zoom)
+  //     zoom >= 16 ? setIsPermanent(true) : setIsPermanent(false)
+  //   })
+  //   console.log('isPermanent', isPermanent)
+
+  //   return null
+  // } 
+
   return (
-    <Marker position={coordinates} icon={icon}>
-      <Tooltip direction='left' offset={[0, 0]} opacity={1} permanent>{ name }</Tooltip>
+    <Marker position={coordinates} icon={blueMarker}>
+      <Tooltip direction='left' offset={[0, 0]} opacity={1} permanent={true}>{ name }</Tooltip>
     </Marker>
   )
 }
