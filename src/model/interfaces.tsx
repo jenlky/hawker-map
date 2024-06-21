05835f52d9849d1daff9633f8e1c2aba36681b0e
Jenssen <jenssen.lee@gmail.com>
2024-06-21 10:55:05 +0800

    Add q1 to q4 cleaning data to hawkerdata interface

--- src/model/interfaces.tsx
+++ src/model/interfaces.tsx
@@ -6,5 +6,13 @@ export interface HawkerData {
   description_myenv?: string, 
   other_works_startdate?: string, 
   other_works_enddate?: string,
+  q1_cleaningenddate?: string,
+  q1_cleaningstartdate?: string,
+  q2_cleaningenddate?: string,
+  q2_cleaningstartdate?: string,
+  q3_cleaningenddate?: string,
+  q3_cleaningstartdate?: string,
+  q4_cleaningenddate?: string,
+  q4_cleaningstartdate?: string,
   index: string
 }
