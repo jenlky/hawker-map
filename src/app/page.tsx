82c185504a763836c0541cf1e835afdba175b12e
Jenssen <jenssen.lee@gmail.com>
2024-06-25 18:23:38 +0800

    Add legend to the map

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,6 +19,16 @@ export default function Home() {
         <p>Welcome to Hawker Map! This website aims to show all the hawkers in Singapore and their closure dates.</p>
       </div>
       <SimpleMap data={hawkerData} />
+      <fieldset className={styles.legendContainer}>
+        <legend> 
+          <img src="/images/marker-icon.png"/>
+          <span className={styles.legendText}>Open Hawker centres</span>
+        </legend>
+        <legend>
+          <img src="/images/marker-icon-red.png"/>
+          <span className={styles.legendText}>Closed Hawker centres</span>
+        </legend>
+      </fieldset>
     </main>
   );
 }
