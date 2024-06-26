ef9e03c195450da318b36bdbce1402c02cc2f2ea
Jenssen <jenssen.lee@gmail.com>
2024-06-26 14:50:05 +0800

    Add height and width to Image component

--- src/components/Legend.tsx
+++ src/components/Legend.tsx
@@ -5,11 +5,11 @@ export default function Legend () {
   return (
     <fieldset className={styles.legendContainer}>
       <legend> 
-        <Image alt="Open Hawker centres marker" src="/images/marker-icon.png" />
+        <Image height={38} width={24} alt="Open Hawker centres marker" src="/images/marker-icon.png" />
         <span className={styles.legendText}>Open Hawker centres</span>
       </legend>
       <legend>
-        <Image alt="Closed Hawker centres marker" src="/images/marker-icon-red.png" />
+        <Image height={38} width={24} alt="Closed Hawker centres marker" src="/images/marker-icon-red.png" />
         <span className={styles.legendText}>Closed Hawker centres</span>
       </legend>
     </fieldset>
