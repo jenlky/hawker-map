514e93a0decd65c34b1b785823d6775f1b0c5ea0
Jenssen <jenssen.lee@gmail.com>
2024-06-26 11:12:25 +0800

    Replace html img tag with next Image component

--- src/components/Legend.tsx
+++ src/components/Legend.tsx
@@ -1,14 +1,15 @@
+import Image from "next/image";
 import styles from "./Legend.module.css";
 
 export default function Legend () {
   return (
     <fieldset className={styles.legendContainer}>
       <legend> 
-        <img src="/images/marker-icon.png"/>
+        <Image alt="Open Hawker centres marker" src="/images/marker-icon.png" />
         <span className={styles.legendText}>Open Hawker centres</span>
       </legend>
       <legend>
-        <img src="/images/marker-icon-red.png"/>
+        <Image alt="Closed Hawker centres marker" src="/images/marker-icon-red.png" />
         <span className={styles.legendText}>Closed Hawker centres</span>
       </legend>
     </fieldset>
