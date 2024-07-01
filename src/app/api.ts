a50ae4b89078cf6345ee3c229477a1acf1584391
Jenssen <jenssen.lee@gmail.com>
2024-07-01 14:17:56 +0800

    Create scrape api endpoint prototype

--- src/app/api.ts
+++ src/app/api.ts
@@ -1,11 +1,9 @@
-const datasetId = "d_bda4baa634dd1cc7a6c7cad5f19e2d68";
-const url =
-  "https://data.gov.sg/api/action/datastore_search?resource_id=" + datasetId;
-
-
 export const API = {
   getHawker: async function () {
     try {
+      const hawkerDatasetId = "d_bda4baa634dd1cc7a6c7cad5f19e2d68";
+      const url = "https://data.gov.sg/api/action/datastore_search?resource_id=" + hawkerDatasetId;
+
       const response = await fetch(url)
       if (!response.ok) {
         throw new Error("Failed to fetch data");
@@ -26,6 +24,17 @@ export const API = {
       return totalRecords
     } catch (error) {
       console.error("Error fetching data:", error);
+    }
+  },
+  scrape: async function (name: any) {
+    try {
+      const url = `http://localhost:4000/scrape?name=${name}`
+      const response: any = await fetch(url)
+      console.log('response', response)
+
+      return response
+    } catch (error) {
+      console.error("Error scraping data:", error);
     }
   }
 }
