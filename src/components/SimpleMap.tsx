ae1ea15cc8981e77316e12e231b551be2b560687
Jenssen <jenssen.lee@gmail.com>
2024-07-04 11:53:05 +0800

    Indentation align

--- src/components/SimpleMap.tsx
+++ src/components/SimpleMap.tsx
@@ -79,7 +79,7 @@ export default function SimpleMap({ data, location }: { data: any, location: any
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-        {listHawker}
+      {listHawker}
     </MapContainer>
   );
 };
