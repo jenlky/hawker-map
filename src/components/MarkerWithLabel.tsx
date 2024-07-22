ef72e0aeda17e88d012875dcbd3e6e2fd6537163
Jenssen <jenssen.lee@gmail.com>
2024-07-22 20:31:14 +0800

    Add both tooltipopen and click handler

--- src/components/MarkerWithLabel.tsx
+++ src/components/MarkerWithLabel.tsx
@@ -2,7 +2,7 @@ import L from "leaflet";
 import { Marker, Tooltip } from "react-leaflet";
 import styles from "./MarkerWithLabel.module.css";
 
-export default function MarkerWithLabel({ data }: { data: any }) {
+export default function MarkerWithLabel({ data, setDisplayWhichHawker }: { data: any, setDisplayWhichHawker: any }) {
   const blueMarker = L.icon({ iconUrl: "/images/marker-icon.png" });
   const redMarker = L.icon({ iconUrl: "/images/marker-icon-red.png" });
 
@@ -23,8 +23,22 @@ export default function MarkerWithLabel({ data }: { data: any }) {
     return
   }
 
+  const tooltipOpenHandler = (e: any) => {
+    console.log('tooltipOpenHandler')
+    console.log(e)
+    const name = e.tooltip.options.children[0].props.children
+    console.log(name)
+  }
+
+  const clickHandler = (e: any) => {
+    console.log('clickHandler')
+    const name = e.target._tooltip.options.children[0].props.children
+    setDisplayWhichHawker(name)
+    console.log(name)
+  }
+  
   return (
-    <Marker position={coordinates} icon={isClosed ? redMarker : blueMarker}>
+    <Marker position={coordinates} icon={isClosed ? redMarker : blueMarker} eventHandlers={{ tooltipopen: tooltipOpenHandler, click: clickHandler }}>
       <Tooltip direction='left' offset={[0, 0]} opacity={1} permanent={false} className={styles.tooltipLabel}>
         <p>{ name }</p>
         <p>{ closureReasons }</p>
