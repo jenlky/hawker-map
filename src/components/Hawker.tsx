0c2fe39b6ff10334e5e96611643684b7fd8f9547
Jenssen <jenssen.lee@gmail.com>
2024-07-29 16:58:38 +0800

    Add css to make image mobile responsive

--- src/components/Hawker.tsx
+++ src/components/Hawker.tsx
@@ -1,15 +1,15 @@
 // use global style
 export default function Hawker ({ hawker, foodRecommendations }: { hawker: any, foodRecommendations: any }) {
   return (
-    <div>
+    <div className="food-recommendations">
       <div className='food-recommendations-header'>
         <h1>{ hawker }</h1>
         <h2>Food recommendations</h2>
       </div>
-      <div>
+      <div className="food-list">
         {foodRecommendations.map((record: any, index: number) => {
           return (
-            <div key={index} className="food-recommendations">
+            <div key={index} className="food-list-items">
               <p>{record.title}</p>
               <p>{record.text}</p>
               {record.image && <img src={record.image}></img>}
