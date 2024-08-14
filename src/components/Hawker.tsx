014f8fdee09af62f4eb46bab9f3e6c4fdfbb8481
Jenssen <jenssen.lee@gmail.com>
2024-08-14 14:38:14 +0800

    Convert date to localdatestring and use next Image

--- src/components/Hawker.tsx
+++ src/components/Hawker.tsx
@@ -1,4 +1,11 @@
-// use global style
+import Image from "next/image";
+
+const options: any = {
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+};
+
 export default function Hawker ({ hawker, foodRecommendations }: { hawker: any, foodRecommendations: any }) {
   return (
     <div className="food-recommendations">
@@ -7,12 +14,33 @@ export default function Hawker ({ hawker, foodRecommendations }: { hawker: any, 
         <h2>Food recommendations</h2>
       </div>
       <div className="food-list">
-        {foodRecommendations.length > 0 && foodRecommendations.map((record: any, index: number) => {
+        {foodRecommendations[0]?.length > 0 && foodRecommendations[0]?.map((record: any, index: number) => {
           return (
-            <div key={index} className="food-list-items">
-              <p>{record.title}</p>
-              <p>{record.text}</p>
-              {record.image && <img src={record.image}></img>}
+            <div key={record?.header}>
+              <div>
+                <h3>{index === 0 && record?.website}</h3>
+                <p>{index === 0 && new Date(record?.date).toLocaleDateString('en-GB', options)}</p>
+              </div>
+              <div className="food-list-items">
+                <p>{record?.header}</p>
+                <p>{record?.text}</p>
+                {record?.image && <Image src={record?.image} alt={record.header} width={300} height={350} />}
+              </div>
+            </div>
+          )
+        })}
+        {foodRecommendations[1]?.length > 0 && foodRecommendations[1]?.map((record: any, index: number) => {
+          return (
+            <div key={record?.header}>
+              <div>
+                <h3>{index === 0 && record?.website}</h3>
+                <p>{index === 0 && new Date(record?.date).toLocaleDateString('en-GB', options)}</p>
+              </div>          
+              <div key={record?.header} className="food-list-items">
+                <p>{record?.header}</p>
+                <p>{record?.text}</p>
+                {record?.image && <Image src={record?.image} alt={record.header} width={300} height={350} />}
+              </div>
             </div>
           )
         })}
