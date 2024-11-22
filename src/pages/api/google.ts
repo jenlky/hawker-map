e0fb02a570ae336baab593da4603f65adc1849d5
Jenssen <jenssen.lee@gmail.com>
2024-11-22 14:36:54 +0800

    Try again without other axios headers

--- src/pages/api/google.ts
+++ src/pages/api/google.ts
@@ -78,10 +78,6 @@ async function scrapeSethLui(url: string) {
         proxy,
         headers: {
             "User-Agent": ua,
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
-            "Accept-Language": "en-US,en;q=0.9",
-            "Connection": "keep-alive",
-            "Referer": "https://www.google.com/",
         },
     });
       console.log('html', html)
