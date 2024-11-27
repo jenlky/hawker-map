08ff0ce63e3a89ac42fe4dd2d6491f8516c55e00
Jenssen <jenssen.lee@gmail.com>
2024-11-27 16:03:30 +0800

    Update puppeteer executablePath

--- src/pages/api/google.ts
+++ src/pages/api/google.ts
@@ -125,7 +125,7 @@ async function scrapeSethLuiPuppeteer(url: string) {
   
     try {
         const browser = await puppeteer.launch({
-            executablePath: '${pwd}/.puppeteer-cache/chrome', // Path to Chrome
+            executablePath: './.puppeteer-cache/chrome', // Path to Chrome
             headless: true,
             args: ['--no-sandbox', '--disable-setuid-sandbox'], // Required for Netlify
         });
